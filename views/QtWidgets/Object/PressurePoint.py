1ca5f8911b794053dade6e3752dfe290b1b31525
IdekiFR <jerome.tran2004@outlook.fr>
2024-05-18 05:52:48 +0900

    Implementing pressur point mesuring on real dimension area + some features

--- views/QtWidgets/Object/PressurePoint.py
+++ views/QtWidgets/Object/PressurePoint.py
@@ -1,9 +1,14 @@
+from PyQt5 import QtGui
+
+
 class PressurePoint:
-    def __init__(self, circle_center_x, circle_center_y, circle_radius, pressure):
+    def __init__(self, circle_center_x, circle_center_y, pressure, border_color = QtGui.QColor(0, 0, 255),
+                 gradient_center= QtGui.QColor(255, 255, 255)):
         self._circle_center_x = circle_center_x
         self._circle_center_y = circle_center_y
-        self._circle_radius = circle_radius
         self._pressure = pressure
+        self._border_color = border_color
+        self._gradient_center = gradient_center
 
     # Getter for circle_center_x
     @property
@@ -15,12 +20,16 @@ class PressurePoint:
     def get_circle_center_y(self):
         return self._circle_center_y
 
-    # Getter for circle_radius
-    @property
-    def get_circle_radius(self):
-        return self._circle_radius
-
-    # Getter for pressure
     @property
     def get_pressure(self):
         return self._pressure
+
+    # Getter for border_color
+    @property
+    def get_border_color(self):
+        return self._border_color
+
+    # Getter for gradient_center
+    @property
+    def get_gradient_center(self):
+        return self._gradient_center
