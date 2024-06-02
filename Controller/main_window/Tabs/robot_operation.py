c0de678c8344249a86c2ba54ff7e9038aa2d355f
IdekiFR <jerome.tran2004@outlook.fr>
2024-06-02 16:25:09 +0900

    Improving Progress Bar (color level)

--- Controller/main_window/Tabs/robot_operation.py
+++ Controller/main_window/Tabs/robot_operation.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-from PyQt5 import uic, QtWidgets
+from PyQt5 import QtWidgets
 from PyQt5.QtCore import QTimer
 
 from views.MainWindow.RobotOperation.FaultDetectionWidget import FaultDetectionWidget
@@ -10,8 +10,9 @@ from views.MainWindow.RobotOperation.Object.PressurePoint import PressurePoint
 from views.MainWindow.RobotOperation.Object.Vector import Vector
 from views.MainWindow.RobotOperation.widget_3dplot import Widget3DPlot
 
+
 class RobotOperation:
-    def __init__(self , main_window):
+    def __init__(self, main_window):
         self.main_window = main_window
         self.active = False
 
@@ -46,29 +47,37 @@ class RobotOperation:
         if not self.active:
             self.active = True
             self.main_window.status_label.setText(f"In Progress")
-            self.main_window.status_label.setStyleSheet("background-color: rgb(0, 170, 0);font-weight: bold;color: 'white';font: "
-                                            "12pt 'MS Shell Dlg 2';")
+            self.main_window.status_label.setStyleSheet(
+                "background-color: rgb(0, 170, 0);font-weight: bold;color: 'white';font: "
+                "12pt 'MS Shell Dlg 2';")
             self.active_time = time.time()
 
+        ####################### Battery Update ########################
         self.main_window.progressBar.setValue(data[0])
 
+        if data[0] <= 10:  # 10% battery
+            self.main_window.progressBar.setStyleSheet("QProgressBar::chunk { background-color: rgb(255, 0, 0); }")
+        elif data[0] <= 50:  # 50% battery
+            self.main_window.progressBar.setStyleSheet("QProgressBar::chunk { background-color: rgb(255, 170, 0); }")
+        else:
+            self.main_window.progressBar.setStyleSheet("QProgressBar::chunk { background-color: rgb(0, 170, 0); }")
+
+        ####################### Temperature Update ########################
         self.main_window.label_13.setText(f"{data[0]}% / {data[1]}˚C")
         self.main_window.label_16.setText(f"{data[0]}% / {data[2]}˚C")
         self.main_window.label_17.setText(f"{data[0]}% / {data[3]}˚C")
 
-
         ####################### 3D Motion Widget ########################
-        self.plot_widget.update_coords((data[4],data[5]), (data[7],data[8]))
-
+        self.plot_widget.update_coords((data[4], -185), (data[7], 185))
 
         ####################### Foot Widget ########################
 
         # Update foot pressure points
-        LeftFootPressurePoints = [PressurePoint(data[10],data[11],data[14],vector=Vector(data[15], data[16]))]
-        RightFootPressurePoints = [PressurePoint(data[12],data[13],data[17],vector=Vector(data[18], data[19]))]
+        LeftFootPressurePoints = [PressurePoint(data[10], data[11], data[14], vector=Vector(data[15], data[16]))]
+        RightFootPressurePoints = [PressurePoint(data[12], data[13], data[17], vector=Vector(data[18], data[19]))]
 
-        self.foot_widget.update_pixmap(LeftFootPressurePoints,RightFootPressurePoints)
-
+        self.foot_widget.update_pixmap(LeftFootPressurePoints, RightFootPressurePoints)
 
         ####################### Fault Detection Widget ########################
-        self.fault_detection_widget.update_fault_list([random.randint(1,16) for _ in range(16)] + [1,3] ,self.current_time)
+        self.fault_detection_widget.update_fault_list([random.randint(1, 16) for _ in range(16)] + [1, 3],
+                                                      self.current_time)
