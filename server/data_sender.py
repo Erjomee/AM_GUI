d97a081fd572a1e6358bf78b918746db9a80587b
IdekiFR <jerome.tran2004@outlook.fr>
2024-06-03 14:52:29 +0900

    Updating data structure

--- server/data_sender.py
+++ server/data_sender.py
@@ -3,6 +3,7 @@ import socket
 import struct
 import threading
 import time
+import zlib
 
 SERVER_IP = "localhost"
 SERVER_PORT = 1818
@@ -10,12 +11,13 @@ SERVER_PORT = 1818
 # Function to send data to the dashboard
 def send_data(ip, port, data):
 
-    # Make a connection with ther server 
+
+    # Make a connection with ther server
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     client_socket.connect((ip, port))
     print("Connected to the server.")
 
-    data_bytes = bytearray(128)
+    data_bytes = bytearray(256)
     data_bytes[2:6] = struct.pack('<i', data["Battery"])  #0
 
     data_bytes[6:10] = struct.pack('<f', data["Temp1"])  #1
@@ -44,14 +46,38 @@ def send_data(ip, port, data):
     data_bytes[82:86] = struct.pack('<f', data["LFoot_time_travel"])  #20
     data_bytes[86:90] = struct.pack('<f', data["RFoot_time_travel"])  #21
 
-    # starting_point_index = 14
-    # for i in range(3,9):
-    #     data_bytes[starting_point_index:starting_point_index+4] = struct.pack('<i', data[i])
-    #     starting_point_index +=4
+    # TEMPERATURE
+    data_bytes[90:94] = struct.pack('<f', data["LH_Abd_Temp"])  #22
+    data_bytes[94:98] = struct.pack('<f', data["LH_Rot_Temp"])  #23
+    data_bytes[98:102] = struct.pack('<f', data["LH_Flex_Temp"])  #24
+    data_bytes[102:106] = struct.pack('<f', data["LK_Temp"])  #25
+    data_bytes[106:110] = struct.pack('<f', data["LA_Lat_Temp"])  #26
+    data_bytes[110:114] = struct.pack('<f', data["LA_Med_Temp"])  #27
 
-    # for point in range(6):
-    #     data_bytes[starting_point_index:starting_point_index+4] = struct.pack('<i', random.randint(-85, 235))
-    #     starting_point_index += 4
+    data_bytes[114:118] = struct.pack('<f', data["RH_Abd_Temp"])  #28
+    data_bytes[118:122] = struct.pack('<f', data["RH_Rot_Temp"])  #29
+    data_bytes[122:126] = struct.pack('<f', data["RH_Flex_Temp"])  #30
+    data_bytes[126:130] = struct.pack('<f', data["RK_Temp"])  #31
+    data_bytes[130:134] = struct.pack('<f', data["RA_Lat_Temp"])  #32
+    data_bytes[134:138] = struct.pack('<f', data["RA_Med_Temp"])  #33
+
+    # AMPERAGE
+    data_bytes[138:142] = struct.pack('<f', data["LH_Abd_Amp"])  #34
+    data_bytes[142:146] = struct.pack('<f', data["LH_Rot_Amp"])  #35
+    data_bytes[146:150] = struct.pack('<f', data["LH_Flex_Amp"])  #36
+    data_bytes[150:154] = struct.pack('<f', data["LK_Amp"])  #37
+    data_bytes[154:158] = struct.pack('<f', data["LA_Lat_Amp"])  #38
+    data_bytes[158:162] = struct.pack('<f', data["LA_Med_Amp"])  #39
+
+    data_bytes[162:166] = struct.pack('<f', data["RH_Abd_Amp"])  #40
+    data_bytes[166:170] = struct.pack('<f', data["RH_Rot_Amp"])  #41
+    data_bytes[170:174] = struct.pack('<f', data["RH_Flex_Amp"])  #42
+    data_bytes[174:178] = struct.pack('<f', data["RK_Amp"])  #43
+    data_bytes[178:182] = struct.pack('<f', data["RA_Lat_Amp"])  #44
+    data_bytes[182:186] = struct.pack('<f', data["RA_Med_Amp"])  #45
+
+    # Compress the data
+    # compressed_data = zlib.compress(data_bytes)
 
     # Send data to the server
     client_socket.sendall(data_bytes)
@@ -126,27 +152,58 @@ def batteryUpdate():
     global info_dict
     for i in range(100):
         # test_data = [battery, w, c , x,y,pressure,x2,y2,pressure2]
+        info_dict["Battery"] -= 1
+        temp = info_dict["Battery"]
+
+        info_dict["LH_Abd_Amp"] = temp
+        info_dict["LH_Rot_Amp"] = temp
+        info_dict["LH_Flex_Amp"] = temp
+        info_dict["LK_Amp"] = temp
+        info_dict["LA_Lat_Amp"] = temp
+        info_dict["LA_Med_Amp"] = temp
+
+        info_dict["RH_Abd_Amp"] = temp
+        info_dict["RH_Rot_Amp"] = temp
+        info_dict["RH_Flex_Amp"] = temp
+        info_dict["RK_Amp"] = temp
+        info_dict["RA_Lat_Amp"] = temp
+        info_dict["RA_Med_Amp"] = temp
         send_data(SERVER_IP, SERVER_PORT, info_dict)
         time.sleep(2)
-        info_dict["Battery"] -= 1
+
 
 
 def temperatureUpdate():
     global info_dict
     for i in range(10000):
         # test_data = [battery, w, c, x,y,pressure,x2,y2,pressure2]
-        send_data(SERVER_IP, SERVER_PORT, info_dict)
-        time.sleep(0.5)
         info_dict["Temp1"] = float(random.randint(0, 200))
         info_dict["Temp2"] = float(random.randint(0, 200))
         info_dict["Temp3"] = float(random.randint(0, 200))
+
+        info_dict["LH_Abd_Temp"] = float(random.randint(0, 200))
+        info_dict["LH_Rot_Temp"] = float(random.randint(0, 200))
+        info_dict["LH_Flex_Temp"] = float(random.randint(0, 200))
+        info_dict["LK_Temp"] = float(random.randint(0, 200))
+        info_dict["LA_Lat_Temp"] = float(random.randint(0, 200))
+        info_dict["LA_Med_Temp"] = float(random.randint(0, 200))
+
+        info_dict["RH_Abd_Temp"] = float(random.randint(0, 200))
+        info_dict["RH_Rot_Temp"] = float(random.randint(0, 200))
+        info_dict["RH_Flex_Temp"] = float(random.randint(0, 200))
+        info_dict["RK_Temp"] = float(random.randint(0, 200))
+        info_dict["RA_Lat_Temp"] = float(random.randint(0, 200))
+        info_dict["RA_Med_Temp"] = float(random.randint(0, 200))
+        send_data(SERVER_IP, SERVER_PORT, info_dict)
+        time.sleep(0.5)
+
+
 
 def feetsPositionUpdate():
     global info_dict
     i = 2
 
     for i in range(100000):
-        time.sleep(1)
         if i % 2 == 0:
             # Foot in the air
             if i == 0:
@@ -171,6 +228,9 @@ def feetsPositionUpdate():
             info_dict["LFootCOP_value"] = random.randint(10, 30)
 
         send_data(SERVER_IP, SERVER_PORT, info_dict)
+        time.sleep(0.05) #max frequencies
+        # time.sleep(1)
+
 
 
 time.sleep(2)
