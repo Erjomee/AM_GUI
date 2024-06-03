d52334ebd9159477aaa1ac81aad6f4332909bbd2
IdekiFR <jerome.tran2004@outlook.fr>
2024-06-03 14:54:54 +0900

    Using Threading + changing receiving data structure

--- server/dummy_server.py
+++ server/dummy_server.py
@@ -1,6 +1,10 @@
 import socket
 import struct
+import zlib
+from concurrent.futures import ThreadPoolExecutor
+
 from PyQt5.QtCore import *
+
 
 class Server(QObject):
     newData = pyqtSignal(list)
@@ -15,6 +19,7 @@ class Server(QObject):
         self.server_socket.listen(5)
 
         self.running = False
+        self.executor = ThreadPoolExecutor(max_workers=5)
 
     def start_server(self):
         self.running = True
@@ -23,30 +28,32 @@ class Server(QObject):
         while self.running:
             try:
                 client_socket, client_address = self.server_socket.accept()
-                # print(f"Connection from {client_address}")
-                self.handle_client(client_socket)
+                self.executor.submit(self.handle_client, client_socket)
             except socket.error as e:
                 if self.running:
                     print(f"Error accepting connection: {e}")
                 else:
                     print("Server is stopped")
                 continue
-   
 
     def handle_client(self, client_socket):
         try:
             while True:
-                received_data = client_socket.recv(128)
+                received_data = client_socket.recv(256)
                 if not received_data:
                     break
 
-                data = struct.unpack('<ifffffffffffffffffffff', received_data[2:90])
+                # # Decompress the data if needed
+                # decompressed_data = zlib.decompress(received_data)
+                data = struct.unpack('<i' + 'f' * 45, received_data[2:186])
                 self.newData.emit(list(data))
+
+        except socket.error as e:
+            print(f"Error handling client: {e}")
         finally:
             client_socket.close()
 
     def stop_server(self):
         self.running = False
         self.server_socket.close()
-
- 
+        self.executor.shutdown(wait=True)
